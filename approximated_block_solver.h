68aca383f0d883028d4057cd384e862a4f0c10f5
Bastian Holst <bastianholst@gmx.de>
2011-02-16 19:12:22 +0100

    Added header for approximated block solver.

---
+++ approximated_block_solver.h
@@ -0,0 +1,21 @@
+/******************************************************************************
+ * Authors:
+ * 2011 Bastian Holst <bastianholst@gmx.de>
+ *****************************************************************************/
+
+#include "linear_algebra.h"
+
+/**
+ * @brief An approximated block solver for linear functions.
+ * 
+ * Calculates an (1 - @p precisison) approximated solution for the problem
+ * find x' in B with:
+ * p^T * A * x' = max{p^T * A * x | x in B}
+ * B = {x in R^(A.width) | sum(x_i) = @param limit, x_i >= 0}
+ * 
+ * @return The approximated solution.
+ */
+double_vector * approximated_block_solver(double_matrix * A,
+                                          double_vector * p,
+                                          uint * limit,
+                                          double precision);
