47ba80eb6c4dc1d20e9f390952ad38daf8b12762
Bastian Holst <bastianholst@gmx.de>
2011-02-16 23:37:28 +0100

    Added vector_matrix_mult and linear algebra tests.

--- linear_algebra.h
+++ linear_algebra.h
@@ -3,10 +3,10 @@
  * 2011 Bastian Holst <bastianholst@gmx.de>
  *****************************************************************************/
 
-#ifndef LINEAR_ALGEBRA_H_
-#define LINEAR_ALGEBRA_H_
-
 #include "packing_list.h"
+
+#ifndef LINEAR_ALGEBRA_H
+#define LINEAR_ALGEBRA_H
 
 // Vectors
 typedef struct {
@@ -44,4 +44,16 @@ typedef struct {
     double * values;
 } double_matrix;
 
+double_vector *alloc_double_vector(unsigned int size);
+void free_double_vector(double_vector *vector);
+
+double_matrix *alloc_double_matrix(unsigned int height, unsigned int width);
+void free_double_matrix(double_matrix *matrix);
+
+/**
+ * Calculates x^T * A
+ */
+double_vector * vector_matrix_mult(double_vector *x, double_matrix *A);
+
 #endif
+
