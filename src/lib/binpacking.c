eda8eea93e238f8844e1aa3f34c6f45179c320ec
hannesg <hag@informatik.uni-kiel.de>
2011-02-17 18:45:32 +0100

    added a method which sorts items

--- src/lib/binpacking.c
+++ src/lib/binpacking.c
@@ -5,3 +5,56 @@
 
 #include "binpacking.h"
 
+packing_list * binpacking(double items[], double epsilon, unsigned int n){
+// 1. extract small items
+// 1.1 count small items
+
+    //
+
+}
+
+void track_sort_items(double items[], unsigned int n, unsigned int positions[])
+{
+    double temp;
+    double pv;
+    unsigned int i = 1, cut = 0, temp_pos;
+    if (n <= 1) {
+        return;
+    }
+    pv = items[0];
+    while (i < n) {
+        if (items[i] >= pv) {
+            // this item should come before the cut
+            // exchange the positions too
+            temp_pos = positions[i];
+            positions[i] = positions[cut];
+            positions[cut] = temp_pos;
+            // exchange the value
+            temp = items[i];
+            items[i] = items[cut];
+            items[cut] = temp;
+
+            cut++;
+        }
+        i++;
+    }
+    if( cut == 0 ){
+        track_sort_items(items + 1, n - 1 , positions + 1);
+    }else{
+        track_sort_items(items, cut, positions);
+        track_sort_items(items + cut, n - cut , positions + cut);
+    }
+}
+
+unsigned int *alloc_positions(unsigned int n)
+{
+    unsigned int *result = malloc(sizeof(unsigned int) * n);
+    unsigned int i = 0;
+    while (i < n) {
+        result[i] = i;
+        i++;
+    }
+    return result;
+}
+
+
--- src/lib/binpacking.h
+++ src/lib/binpacking.h
@@ -1,9 +1,36 @@
 /******************************************************************************
  * Authors:
  * 2011 Bastian Holst <bastianholst@gmx.de>
+ * 2011 Hannes Georg <hannes.georg@googlemail.com>
  *****************************************************************************/
 
+#ifndef BINPACKING_H_
+#define BINPACKING_H_
+
+/** @file binpacking.h
+ *  @brief Approximative Binpacking algorithm
+ */
+
 #include "packing_list.h"
+#include "linear_algebra.h"
+
 
 packing_list * binpacking(double items[], double epsilon, unsigned int n);
 
+/** @brief Sorts the given items an writes a list ( new_position => old_position ) into positions.
+ *
+ *  @param items the items
+ *  @param n the number of items
+ *  @param positions the positions
+ *
+ */
+void track_sort_items(double items[], unsigned int n, unsigned int positions[]);
+
+/** @brief Creates a positions list to be used with track_sort_items.
+ *
+ * @param n the number of items
+ * @return a field of n entries filled with 0 to n-1
+ *
+ */
+unsigned int *alloc_positions(unsigned int n);
+#endif
