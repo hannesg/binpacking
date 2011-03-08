357991926071e0423551abd9e9908e67ece30429
Bastian Holst <bastianholst@gmx.de>
2011-03-08 16:55:56 +0100

    There it is.

---
+++ src/lib/approximate_max_min_resource_sharing_p.h
@@ -0,0 +1,10 @@
+/******************************************************************************
+ * Authors:
+ * 2011 Bastian Holst <bastianholst@gmx.de>
+ *****************************************************************************/
+
+#include "linear_algebra.h"
+
+double find_optimum(double_vector *b, double t);
+
+double calculate_optimum_condition(double_vector *b, double theta, double t);
