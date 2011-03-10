896d0048eef6c3616c70afe4a2f987d34acb7b63
Bastian Holst <bastianholst@gmx.de>
2011-03-10 15:27:04 +0100

    Corrected lp_solver.

--- src/lib/approximate_lp_solver.c
+++ src/lib/approximate_lp_solver.c
@@ -18,15 +18,13 @@ double_vector *approximate_lp_solver(uint_matrix *A,
                                      double precision,
                                      unsigned int limit)
 {
-    int minimum = 0;
+    int minimum = 1;
     int maximum = limit;
     
     double_matrix *matrix = uint_matrix_vector_division(A, b);
     int medium;
     do {
-        printf("Minimum: %i, maximum: %i\n", minimum, maximum);
         medium =  (minimum + maximum)/2; // This does not fail because limit is small.
-        printf("Medium: %i\n", medium);
         max_min_resource_sharing_solution *solution
             = approximate_max_min_resource_sharing(matrix,
                                                    medium,
@@ -46,6 +44,8 @@ double_vector *approximate_lp_solver(uint_matrix *A,
                                                maximum,
                                                precision);
     free_double_vector(end_solution->function_solution);
+    
+    double_vector *solution_vector = end_solution->vector;
     free(end_solution);
-    return end_solution->vector;
+    return solution_vector;
 }
--- src/tests/approximate_lp_solver.c
+++ src/tests/approximate_lp_solver.c
@@ -25,13 +25,13 @@ void Test_approximate_lp_solver(CuTest *tc)
     b->values[0] = 2;
     b->values[1] = 1;
     
-//     double_vector *x = approximate_lp_solver(A, b, 0.25, 10);
+    double_vector *x = approximate_lp_solver(A, b, 0.25, 10);
     
-//     CuAssertDblEquals(tc, 0.5, x->values[0], DBL_EPSILON);
-//     CuAssertDblEquals(tc, 0, x->values[1], DBL_EPSILON);
-//     CuAssertDblEquals(tc, 0, x->values[2], DBL_EPSILON);
+    CuAssertDblEquals(tc, 1.0, x->values[0], DBL_EPSILON);
+    CuAssertDblEquals(tc, 0, x->values[1], DBL_EPSILON);
+    CuAssertDblEquals(tc, 0, x->values[2], DBL_EPSILON);
     
     free_uint_matrix(A);
     free_uint_vector(b);
-//     free_double_vector(x);
+    free_double_vector(x);
 }
